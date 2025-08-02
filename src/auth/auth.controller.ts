3d44d1ee2cdda7842176d4a4e799a91d37f8928e
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-02 10:55:43 -0700

    feat: setup jwt module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,8 +10,10 @@ export class AuthController {
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
-  @Post('/signIn')
-  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  @Post('/signin')
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthController } from './auth.controller';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   providers: [AuthService, UserRepository],
   controllers: [AuthController],
 })
