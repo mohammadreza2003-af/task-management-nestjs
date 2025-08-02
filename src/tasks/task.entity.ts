942061cebd5d698f5df15a2f12638a238b90a215
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-02 12:06:39 -0700

    feat: use global transform interceptor

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { User } from 'src/auth/user.entity';
 import { TaskStatus } from './task-status.enum';
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -17,5 +18,8 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({
+    toPlainOnly: true,
+  })
   user: User;
 }
