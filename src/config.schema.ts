c902da582d7afde3c165561ffbcde74ef4d86fdf
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-06 00:02:13 -0700

    feat: add validation schema on env

---
+++ src/config.schema.ts
@@ -0,0 +1,10 @@
+import * as JOI from '@hapi/joi';
+
+export const configValidationSchema = JOI.object({
+  STAGE: JOI.string().required(),
+  DB_HOST: JOI.string().required(),
+  DB_DATABASE: JOI.string().required(),
+  DB_USERNAME: JOI.string().required(),
+  DB_PASSWORD: JOI.string().required(),
+  DB_PORT: JOI.number().default(5432).required(),
+});
